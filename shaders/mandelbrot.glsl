0f029ff2a386d85f3bf02f95af0e4a77a13c4748
Loïc Pottier <loic.pottier@live.fr>
2023-11-28 17:20:35 +0100

    rework: changeConfig fn

--- shaders/mandelbrot.glsl
+++ shaders/mandelbrot.glsl
@@ -9,8 +9,10 @@ out vec4 FragColor;
 void main() {
   vec2 uv = gl_FragCoord.xy / resolution.xy;
 
-  // Should try this and see the fractal placement
+  // TODO: Should try this and see the fractal placement
   // vec2 madelbrotCoord = uv
+
+  // TODO: To test better pricision, simply replacing vec2 with dvec2 for mandelbrotCoord and z may be enought
   vec2 mandelbrotCoord = vec2(uv.x * 3.5 - 2.5, uv.y * 2.0 - 1.0);
 
   vec2 z = mandelbrotCoord;
--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(Constants::WIDTH, Constants::HEIGHT), "Fractal Explorer");
-  window.setFramerateLimit(60);
+  window.setFramerateLimit(240);
   if (!ImGui::SFML::Init(window)) {
     std::cerr << "Error initializing SFML window" << std::endl;
   }
@@ -31,7 +31,6 @@ int main() {
   bool ui = true;
 
   while (window.isOpen()) {
-    bool configChanged = false;
     sf::Event event;
     while (window.pollEvent(event)) {
       switch (event.type) {
@@ -41,6 +40,13 @@ int main() {
         case sf::Event::KeyPressed:
           if (event.key.code == sf::Keyboard::Escape) { window.close(); }
           if (event.key.code == sf::Keyboard::Enter) { ui = !ui; }
+          break;
+        case sf::Event::MouseButtonPressed:
+          if (!ImGui::GetIO().WantCaptureMouse) {
+            if (event.mouseButton.button == sf::Mouse::Left && !ImGui::IsWindowHovered()) {
+              std::cout << "Left mouse button clicked!" << std::endl;
+            }
+          }
           break;
         default:
           break;
@@ -53,36 +59,55 @@ int main() {
     // ImGui frame
     if (ui) {
       ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_Always);
-      ImGui::SetNextWindowSize(ImVec2(275, 310), ImGuiCond_Always);
+      ImGui::SetNextWindowSize(ImVec2(275, 320), ImGuiCond_Always);
       ImGui::Begin("Configuration");
 
-      ImGui::Text("FractalType");
-      configChanged = ImGui::Combo("##FractalType", &type, FRACTAL_NAMES.data(), static_cast<int>(SHADERS_PATH.size())) || configChanged;
+      ImGui::Text("Fractal Type");
+      if (ImGui::Combo("##FractalType", &type, FRACTAL_NAMES.data(), static_cast<int>(SHADERS_PATH.size()))) {
+        std::cout << "Fractal type changed." << std::endl;
+        //renderer.changeShader();
+      }
 
       ImGui::Text("Iterations");
-      configChanged = ImGui::SliderInt("##Iterations", &maxIter, 1, 10000) || configChanged;
+      if (ImGui::SliderInt("##Iterations", &maxIter, 1, 10000)) {
+        std::cout << "Maximum iterations changed." << std::endl;
+        //renderer.changeIteration();
+      }
 
       ImGui::Text("Position");
-      configChanged = ImGui::InputFloat("X##Position", &position.x) || configChanged;
-      configChanged = ImGui::InputFloat("Y##Position", &position.y) || configChanged;;
+      if (ImGui::InputFloat("X##Position", &position.x) || ImGui::InputFloat("Y##Position", &position.y)) {
+        std::cout << "Position changed." << std::endl;
+        //renderer.changePosition();
+      }
 
       ImGui::Text("Zoom");
-      configChanged = ImGui::InputFloat("##Zoon", &zoom) || configChanged;
+      if (ImGui::InputFloat("##Zoon", &zoom)) {
+        std::cout << "Zoom changed." << std::endl;
+        //renderer.changeZoom();
+      }
 
       ImGui::Text("Color palette");
-      configChanged = ImGui::Combo("##ColorPalette", &palette, PALETTE_NAMES.data(), static_cast<int>(PALETTES.size())) || configChanged;
+      if (ImGui::Combo("##ColorPalette", &palette, PALETTE_NAMES.data(), static_cast<int>(PALETTES.size()))) {
+        std::cout << "Color palette changed." << std::endl;
+        //renderer.changeColorPalette();
+      }
 
       ImGui::Text("Color repetition");
-      configChanged = ImGui::SliderFloat("##ColorRepetition", &repetition, 0, 10) || configChanged;
+      if (ImGui::SliderFloat("##ColorRepetition", &repetition, 0, 10)) {
+        std::cout << "Color repetition rate changed." << std::endl;
+        //renderer.changeRepetition();
+      }
 
       ImGui::TextColored(ImVec4(0.9f, 0.9f, 0.9f, 0.7f), "Press enter to hide/show this panel.");
-
       ImGui::End();
-    }
 
-    if (configChanged) {
-      std::cout << "Configuration changed" << std::endl;
-      // renderer.changeConfig();
+      // Create another window for FPS
+      ImGui::SetNextWindowPos(ImVec2(1650, 885), ImGuiCond_Always);
+      ImGui::SetNextWindowSize(ImVec2(250, 75), ImGuiCond_Always);
+      ImGui::Begin("Statistics");
+      ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
+      ImGui::Text("Last frame computation: %.3f ms", ImGui::GetIO().DeltaTime * 1000);
+      ImGui::End();
     }
 
     // Rendering
